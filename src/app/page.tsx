ee17766c4dcc3c750d4cd4c9f53535bfd7ead902
ay8s <contact@andydev.co.uk>
2024-01-20 13:19:38 -0800

    Missed one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
               Motorsport <span className="text-red-600">Calendars</span>
             </h2>
-            <h3 class='text-xl md:text-3xl mt-10 text-black'>Coming Soon</h3>
+            <h3 className='text-xl md:text-3xl mt-10 text-black'>Coming Soon</h3>
           </div>
         </div>
       </div>
