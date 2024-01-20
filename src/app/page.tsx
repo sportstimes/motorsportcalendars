353e1b492e3e0ed6718e57aeb444145bbb14cbe9
ay8s <contact@andydev.co.uk>
2024-01-20 13:15:01 -0800

    Correct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div class="flex items-center justify-center h-screen bg-gray-200">
-      <div class="container">
-        <div class="">
-          <div class="text-center">
-            <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-              Motorsport <span class="text-red-600">Calendars</span>
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <div className="container">
+        <div className="">
+          <div className="text-center">
+            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Motorsport <span className="text-red-600">Calendars</span>
             </h2>
             <h3 class='text-xl md:text-3xl mt-10 text-black'>Coming Soon</h3>
           </div>
