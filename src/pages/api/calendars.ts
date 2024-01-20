d0b854fac7772e21d6a36c89c41aba636a7cb908
ay8s <contact@andydev.co.uk>
2024-01-20 13:31:48 -0800

    Endpoint

--- src/pages/api/calendars.ts
+++ src/pages/api/calendars.ts
@@ -1,12 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
  
 type ResponseData = {
-  message: string
+  calendars: [Calendar]
+}
+
+type Calendar = {
+  name: string
+  endpointURL: string
+	calendarURL: string
 }
  
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
+  res.status(200).json({ 
+		calendars: [
+			{
+				name: 'Formula 1',
+				endpointURL: 'https://f1calendar.com/api/year/2024',
+				calendarURL: 'https://files-f1.motorsportcalendars.com/f1-calendar_p1_p2_p3_qualifying_sprint_gp.ics',
+			}
+		] 
+	})
 }
