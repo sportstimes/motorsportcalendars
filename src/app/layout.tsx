5d8057ff8657103b756c077e9c602f42c52432f5
ay8s <contact@andydev.co.uk>
2024-01-20 13:04:51 -0800

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Motorsport Calendars",
+  description: "Calendars for Formula 1, Formula 2, Formula 3, F1 Academy, Formula E, Extreme E",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div class="flex items-center justify-center h-screen bg-gray-200">
+      <div class="container">
+        <div class="">
+          <div class="text-center">
+            <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Motorsport <span class="text-red-600">Calendars</span>
+            </h2>
+            <h3 class='text-xl md:text-3xl mt-10 text-black'>Coming Soon</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
