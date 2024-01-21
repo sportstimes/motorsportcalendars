e5d96499aa38d50950fe8923b7dcb5968a444a43
ay8s <contact@andydev.co.uk>
2024-01-21 11:49:46 -0800

    Add more calendars

--- src/pages/api/calendars.ts
+++ src/pages/api/calendars.ts
@@ -19,7 +19,37 @@ export default function handler(
 			{
 				name: 'Formula 1',
 				endpointURL: 'https://f1calendar.com/api/year/2024',
-				calendarURL: 'https://files-f1.motorsportcalendars.com/f1-calendar_p1_p2_p3_qualifying_sprint_gp.ics',
+				calendarURL: 'webcal://files-f1.motorsportcalendars.com/f1-calendar_p1_p2_p3_qualifying_sprint_gp.ics',
+			},
+			{
+				name: 'F1 Academy',
+				endpointURL: 'https://f1academycalendar.com/api/year/2024',
+				calendarURL: 'webcal://files-f1-academy.motorsportcalendars.com/f1-academy-calendar_fp1_fp2_qualifying1_qualifying2_race1_race2_race3.ics',
+			},
+			{
+				name: 'Formula 2',
+				endpointURL: 'https://f2cal.com/api/year/2024',
+				calendarURL: 'webcal://files-f2.motorsportcalendars.com/f2-calendar_p_q_sprint_feature.ics',
+			},
+			{
+				name: 'Formula 3',
+				endpointURL: 'https://f2cal.com/api/year/2024',
+				calendarURL: 'webcal://files-f2.motorsportcalendars.com/f2-calendar_p_q_sprint_feature.ics',
+			},
+			{
+				name: 'Formula E',
+				endpointURL: 'https://formulaecal.com/api/year/2024',
+				calendarURL: 'webcal://files-fe.motorsportcalendars.com/fe-calendar_p1_p2_p3_qualifying_race.ics',
+			},
+			{
+				name: 'Extreme E',
+				endpointURL: 'https://extremeecalendar.com/api/year/2024',
+				calendarURL: 'webcal://files-extremee.motorsportcalendars.com/extremee-calendar_shakedown_qualifying1_qualifying2_undefined_undefined.ics',
+			},
+			{
+				name: 'MotoGP',
+				endpointURL: 'https://motogpcal.com/api/year/2024',
+				calendarURL: 'webcal://files-motogp.motorsportcalendars.com/motogp-calendar_p1_p2_p3_p4_qualifying1_qualifying2_sprint_race.ics',
 			}
 		] 
 	})
